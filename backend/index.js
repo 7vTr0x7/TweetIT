f1cc6a63a135ee18f2d6f053fc16cfc94494fa2a
7vTr0x7 <7vTrox7@gmail.com>
2024-09-10 12:37:50 +0530

    TweetIT - backend - getPosts

--- backend/index.js
+++ backend/index.js
@@ -15,3 +15,26 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 initializeDatabase();
+
+const getPosts = async () => {
+  try {
+    const posts = await SocialPosts.find();
+    return posts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.get("/api/posts", async (req, res) => {
+  try {
+    const posts = await getPosts();
+
+    if (posts && posts.length > 0) {
+      res.json(posts);
+    } else {
+      res.status(404).json({ error: `Posts not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: `Failed to get posts error: ${error}` });
+  }
+});
