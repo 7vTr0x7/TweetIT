b1ab27f38f2a1fb419fb7ab9e281d900082b1027
7vTr0x7 <7vTrox7@gmail.com>
2024-09-10 12:28:25 +0530

    TweetIT - backend - models - post.model

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,7 +1,36 @@
 const mongoose = require("mongoose");
 
-const SocialPostSchema = new mongoose.Schema({}, { timestamps: true });
+const SocialPostSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+    },
+    videoUrl: {
+      type: String,
+    },
+    likesCount: {
+      type: Number,
+      required: true,
+    },
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "SocialUser",
+        },
+        comment: {
+          type: String,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-const SocialPosts = mongoose.model("SocialPost", SocialPostSchema);
+const SocialPosts = mongoose.model("SocialPosts", SocialPostSchema);
 
 module.exports = SocialPosts;
