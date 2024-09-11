6eb16950814fef473bbafc93c2b1a8a1306c4afb
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 14:32:32 +0530

    Nav

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOutlineExplore } from "react-icons/md";
+import { IoBookmarkOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <>
+      <nav>
+        <ul
+          className=" fw-semibold fs-6 "
+          style={{ listStyle: "none", padding: "0px" }}>
+          <li className="mt-3">
+            <AiOutlineHome style={{ fontSize: "25px", marginBottom: "5px" }} />
+            <span className="px-3">Home</span>
+          </li>
+          <li className="mt-3">
+            <MdOutlineExplore
+              style={{ fontSize: "25px", marginBottom: "5px" }}
+            />
+            <span className=" px-3">Explore</span>
+          </li>
+          <li className="mt-3">
+            <IoBookmarkOutline
+              style={{ fontSize: "25px", marginBottom: "5px" }}
+            />
+            <span className=" px-3">Bookmark</span>
+          </li>
+          <li className="mt-3">
+            <CgProfile style={{ fontSize: "25px", marginBottom: "5px" }} />
+            <span className=" px-3">Profile</span>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,16 +4,16 @@ import Nav from "../../components/Nav";
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Header />
-      <main className="my-4 bg-bg-body-tertiary container">
+      <main className="my-4 bg-body-tertiary container px-4">
         <div className="row">
           <div className="col-md-3 ">
             <Nav />
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
