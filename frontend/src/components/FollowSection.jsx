7ab4b5a8ea60058a935dfeffc138ffab3fbe509a
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 15:00:29 +0530

    followSection

--- frontend/src/components/FollowSection.jsx
+++ frontend/src/components/FollowSection.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+import { FiSearch } from "react-icons/fi";
+
 const FollowSection = () => {
   return (
     <>
       <div className="p-3">
-        <input className="form-control" placeholder="Search" />
+        <div
+          className="d-flex px-2 py-1 rounded-2"
+          style={{ backgroundColor: "white" }}>
+          <FiSearch style={{ fontSize: "25px", marginTop: "5px" }} />
+          <input
+            className="form-control mx-1 border-0 bg-transparent"
+            placeholder="Search"
+          />
+        </div>
 
         <div className="card my-3 ">
           <p className="card-header fw-semibold text-center">
