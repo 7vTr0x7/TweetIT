24b93f893a23804a54df390ed315f0f043d05b35
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 17:37:29 +0530

    redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 
+import { Provider } from "react-redux";
+
 import App from "./App.jsx";
+import store from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
