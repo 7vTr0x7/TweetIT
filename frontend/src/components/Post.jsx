ab5d64aacc74d6d5a01f5e41a78da87aefeb45ee
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 21:11:09 +0530

    Post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,14 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { HiDotsVertical } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
+
+import { FaRegHeart } from "react-icons/fa";
+import { IoMdShare } from "react-icons/io";
+import { FaRegComment } from "react-icons/fa";
+import { FaRegBookmark } from "react-icons/fa6";
 
 const Post = ({ post }) => {
+  const [isOptionOpen, setIsOptionOpen] = useState(false);
+  const date = new Date(post.createdAt);
+
   return (
     <>
-      <div className="mt-3 p-4 rounded-3" style={{ backgroundColor: "white " }}>
-        <div>
-          <img
-            src={post.user.avatarUrl}
-            style={{ height: "50px", width: "50px", borderRadius: "100%" }}
-          />
+      <div
+        className=" mt-3 p-3 rounded-3 "
+        style={{ backgroundColor: "white " }}>
+        <div className="d-flex justify-content-between align-content-center">
+          <div className="d-flex ">
+            <img
+              src={post.user.avatarUrl}
+              style={{ height: "50px", width: "50px", borderRadius: "100%" }}
+            />
+            <div>
+              <span className="m-0 px-3 fw-bold ">{post.user.userName}</span>
+              <small className="m-0  ">{post.user.userAt}</small>
+              <p className="m-0  px-3">
+                <small>{date.toLocaleString()}</small>
+              </p>
+            </div>
+          </div>
+          <span
+            style={{ fontSize: "20px", cursor: "pointer" }}
+            onClick={() => setIsOptionOpen((prev) => !prev)}>
+            {isOptionOpen ? (
+              <RxCross2 style={{ fontSize: "25px" }} />
+            ) : (
+              <HiDotsVertical />
+            )}
+          </span>
+        </div>
+        <div className="position-relative">
+          {isOptionOpen && (
+            <div
+              className="position-absolute fw-semibold shadow-lg px-3 py-2 rounded-3 z-1"
+              style={{
+                backgroundColor: "white",
+                right: "0px",
+                top: "0px",
+                cursor: "pointer",
+              }}>
+              <p className="m-0">Edit</p>
+              <p className="m-0">Delete</p>
+            </div>
+          )}
+        </div>
+
+        <div className="my-4 px-2">
+          <p>{post.description}</p>
+        </div>
+        <div className="d-flex justify-content-between px-3 align-content-center fw-semibold">
+          <p className="m-0">
+            <span>
+              <FaRegHeart />
+            </span>
+            <span className=" px-1 ">{post.likesCount}</span>
+          </p>
+          <p className="m-0">
+            <span>
+              <FaRegComment />
+            </span>
+          </p>
+          <span>
+            <FaRegBookmark />
+          </span>
+          <span>
+            <IoMdShare />
+          </span>
         </div>
       </div>
     </>
--- frontend/src/pages/Home/features/Feed.jsx
+++ frontend/src/pages/Home/features/Feed.jsx
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { readUser } from "../../Profile/userSlice";
 import Post from "../../../components/Post";
 
-const Main = () => {
+const Feed = () => {
   const [isPostOpen, setIsPostOpen] = useState(false);
   const [isFilter, setIsFilter] = useState(false);
   const [filter, setFilter] = useState("Latest");
@@ -47,86 +47,93 @@ const Main = () => {
   }, [user]);
 
   return (
-    <div className="position-relative">
-      <div
-        onClick={() => setIsPostOpen(true)}
-        className="my-3 py-2 px-3 d-flex rounded-3"
-        style={{ backgroundColor: "white" }}>
-        <img
-          src={user ? user.avatarUrl : "https://via.placeholder.com/50"}
-          style={{ height: "50px", width: "50px", borderRadius: "100%" }}
-        />
-        <p
-          className="m-0 px-4 fw-semibold"
-          style={{ position: "relative", top: "13px" }}>
-          What is happening?
-        </p>
-      </div>
-      {isPostOpen && (
-        <div className="d-flex justify-content-center ">
-          <div
-            className="p-3 rounded-3 position-absolute z-2 shadow-lg"
-            style={{ backgroundColor: "white", width: "70%", top: "20px" }}>
-            <span className="px-1 py-3" onClick={() => setIsPostOpen(false)}>
-              <FaArrowLeftLong style={{ fontSize: "20px" }} />
-            </span>
-            <div>
-              <textarea
-                className="form-control my-3"
-                placeholder="What is Happening?"
-                rows="5"></textarea>
-            </div>
-            <div className="d-flex justify-content-between">
+    <>
+      <div className="position-relative mb-5">
+        <div
+          onClick={() => setIsPostOpen(true)}
+          className="my-3 py-2 px-3 d-flex rounded-3"
+          style={{ backgroundColor: "white" }}>
+          <img
+            src={user ? user.avatarUrl : "https://via.placeholder.com/50"}
+            style={{ height: "50px", width: "50px", borderRadius: "100%" }}
+          />
+          <p
+            className="m-0 px-4 fw-semibold"
+            style={{ position: "relative", top: "13px" }}>
+            What is happening?
+          </p>
+        </div>
+        {isPostOpen && (
+          <div className="d-flex justify-content-center ">
+            <div
+              className="p-3 rounded-3 position-absolute z-2 shadow-lg"
+              style={{
+                backgroundColor: "white",
+                width: "70%",
+                top: "20px",
+                zIndex: 10,
+              }}>
+              <span className="px-1 py-3" onClick={() => setIsPostOpen(false)}>
+                <FaArrowLeftLong style={{ fontSize: "20px" }} />
+              </span>
               <div>
-                <FaRegImage style={{ fontSize: "20px" }} />
-                <MdGif style={{ fontSize: "30px", marginLeft: "10px" }} />
+                <textarea
+                  className="form-control my-3"
+                  placeholder="What is Happening?"
+                  rows="5"></textarea>
               </div>
-              <button className="btn btn-light fw-semibold">Post</button>
+              <div className="d-flex justify-content-between">
+                <div>
+                  <FaRegImage style={{ fontSize: "20px" }} />
+                  <MdGif style={{ fontSize: "30px", marginLeft: "10px" }} />
+                </div>
+                <button className="btn btn-light fw-semibold">Post</button>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
-
-      <div className="position-relative" style={{ backgroundColor: "white" }}>
-        <div className="d-flex justify-content-between  my-4 px-3 py-2">
-          <p className="m-0 fw-semibold">{filter} Post</p>
-          <span onClick={() => setIsFilter((prev) => !prev)}>
-            <FiSliders />
-          </span>
-        </div>
-        {isFilter && (
-          <div
-            className="position-absolute px-3 py-2 "
-            style={{ right: "10px", top: "30px", cursor: "pointer" }}>
-            <ul className="list-group fw-semibold z-2 shadow-lg">
-              <li
-                className="list-group-item"
-                onClick={() => setFilter("Latest")}>
-                Latest
-              </li>
-              <li
-                className="list-group-item"
-                onClick={() => setFilter("Trending")}>
-                Trending
-              </li>
-              <li
-                className="list-group-item"
-                onClick={() => setFilter("Oldest")}>
-                Oldest
-              </li>
-            </ul>
           </div>
         )}
-      </div>
-      {posts &&
-        posts.length > 0 &&
-        posts.map((post) => (
-          <div key={post._id}>
-            <Post post={post} />
+
+        <div className="position-relative" style={{ backgroundColor: "white" }}>
+          <div className="d-flex justify-content-between  my-4 px-3 py-2">
+            <p className="m-0 fw-semibold">{filter} Post</p>
+            <span onClick={() => setIsFilter((prev) => !prev)}>
+              <FiSliders />
+            </span>
           </div>
-        ))}
-    </div>
+          {isFilter && (
+            <div
+              className="position-absolute px-3 py-2 "
+              style={{ right: "10px", top: "30px", cursor: "pointer" }}>
+              <ul className="list-group fw-semibold z-2 shadow-lg">
+                <li
+                  className="list-group-item"
+                  onClick={() => setFilter("Latest")}>
+                  Latest
+                </li>
+                <li
+                  className="list-group-item"
+                  onClick={() => setFilter("Trending")}>
+                  Trending
+                </li>
+                <li
+                  className="list-group-item"
+                  onClick={() => setFilter("Oldest")}>
+                  Oldest
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+        {posts &&
+          posts.length > 0 &&
+          posts.map((post) => (
+            <div key={post._id}>
+              <Post post={post} />
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
-export default Main;
+export default Feed;
