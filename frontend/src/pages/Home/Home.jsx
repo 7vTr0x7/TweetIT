ca41c363ff9301d4e378cc0e5fe9fbf06c8f0d4c
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 14:07:50 +0530

    Nav

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "../../components/Header";
+import Nav from "../../components/Nav";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <main className="container bg-bg-body-tertiary"></main>
+      <main className="my-4 bg-bg-body-tertiary container">
+        <div className="row">
+          <div className="col-md-3 ">
+            <Nav />
+          </div>
+        </div>
+      </main>
     </>
   );
 };
