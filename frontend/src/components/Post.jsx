924d1ea31af688284910767c568fb033e2b6acc5
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 19:14:33 +0530

    Post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({ post }) => {
+  return (
+    <>
+      <div className="mt-3 p-4 rounded-3" style={{ backgroundColor: "white " }}>
+        <div>
+          <img
+            src={post.user.avatarUrl}
+            style={{ height: "50px", width: "50px", borderRadius: "100%" }}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Post;
