a1e2fa7efa61727567e760a5f258cb2aa8e7ae46
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 14:48:10 +0530

    Main

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/Header";
 import Nav from "../../components/Nav";
 import FollowSection from "../../components/FollowSection";
+import Main from "./features/Main";
 
 const Home = () => {
   return (
@@ -12,7 +13,9 @@ const Home = () => {
           <div className="col-md-3 ">
             <Nav />
           </div>
-          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Main />
+          </div>
           <div className="col-md-3">
             <FollowSection />
           </div>
