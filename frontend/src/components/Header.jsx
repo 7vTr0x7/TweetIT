00a54c79e7234ef6def5107a2be2cb6744dfff62
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 13:46:34 +0530

    Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="container py-3">
+      <h3 className="text-dark">
+        Tweet<span className="text-primary">IT</span>
+      </h3>
+    </header>
+  );
+};
+
+export default Header;
