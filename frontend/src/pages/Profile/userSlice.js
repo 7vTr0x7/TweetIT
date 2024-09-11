93fbc02847f6514e4ab8d43465f3939fae878a1b
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 17:53:48 +0530

    userSlice

--- frontend/src/pages/Profile/userSlice.js
+++ frontend/src/pages/Profile/userSlice.js
@@ -3,28 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    user:{
-        
-    }
+    user: {},
   },
 });
-
-
-{
-    "_id": "66e183147b64fafc1e2fa38a",
-    "avatarUrl": "https://i.pravatar.cc/300?img=6",
-    "userName": "vTr0x -_-",
-    "userAt": "@vtrox_0",
-    "bio": "Writer and environmental activist. Dedicated to making a difference.",
-    "followers": [],
-    "following": [],
-    "posts": [
-        "66e18418fd7ec75ef6d5ba62",
-        "66e18426fd7ec75ef6d5ba66"
-    ],
-    "bookmarks": [],
-    "likedPosts": [],
-    "createdAt": "2024-09-11T11:46:28.229Z",
-    "updatedAt": "2024-09-11T11:51:02.174Z",
-    "__v": 2
-}
