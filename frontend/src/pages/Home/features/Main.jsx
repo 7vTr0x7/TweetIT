f32b043bcbdfe1746f0e14636198ece0efdcdd80
7vTr0x7 <7vTrox7@gmail.com>
2024-09-11 17:11:45 +0530

    Main

--- frontend/src/pages/Home/features/Main.jsx
+++ frontend/src/pages/Home/features/Main.jsx
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { FaRegImage } from "react-icons/fa6";
 import { MdGif } from "react-icons/md";
+import { FiSliders } from "react-icons/fi";
 
 const Main = () => {
-  const [isPost, setIsPost] = useState(false);
+  const [isPostOpen, setIsPostOpen] = useState(false);
+  const [isFilter, setIsFilter] = useState(false);
+  const [filter, setFilter] = useState("Latest");
 
   return (
     <div className="position-relative">
       <div
-        onClick={() => setIsPost(true)}
-        className="my-4 py-2 px-3 d-flex rounded-3"
+        onClick={() => setIsPostOpen(true)}
+        className="my-3 py-2 px-3 d-flex rounded-3"
         style={{ backgroundColor: "white" }}>
         <img
           src="https://via.placeholder.com/50"
@@ -23,12 +26,12 @@ const Main = () => {
           What is happening?
         </p>
       </div>
-      {isPost && (
+      {isPostOpen && (
         <div className="d-flex justify-content-center ">
           <div
-            className="p-3 rounded-3 position-absolute"
+            className="p-3 rounded-3 position-absolute z-2 shadow-sm"
             style={{ backgroundColor: "white", width: "70%", top: "20px" }}>
-            <span className="px-1 py-3" onClick={() => setIsPost(false)}>
+            <span className="px-1 py-3" onClick={() => setIsPostOpen(false)}>
               <FaArrowLeftLong style={{ fontSize: "20px" }} />
             </span>
             <div>
@@ -47,6 +50,38 @@ const Main = () => {
           </div>
         </div>
       )}
+
+      <div className="position-relative" style={{ backgroundColor: "white" }}>
+        <div className="d-flex justify-content-between  my-4 px-3 py-2">
+          <p className="m-0 fw-semibold">{filter} Post</p>
+          <span onClick={() => setIsFilter((prev) => !prev)}>
+            <FiSliders />
+          </span>
+        </div>
+        {isFilter && (
+          <div
+            className="position-absolute px-3 py-2 "
+            style={{ right: "10px", top: "30px", cursor: "pointer" }}>
+            <ul className="list-group fw-semibold z-2 shadow-sm">
+              <li
+                className="list-group-item"
+                onClick={() => setFilter("Latest")}>
+                Latest
+              </li>
+              <li
+                className="list-group-item"
+                onClick={() => setFilter("Trending")}>
+                Trending
+              </li>
+              <li
+                className="list-group-item"
+                onClick={() => setFilter("Oldest")}>
+                Oldest
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
