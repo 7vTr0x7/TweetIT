5cfaadbb0b18b74096fab47868800efa7fe11db9
7vTr0x7 <7vTrox7@gmail.com>
2024-09-14 15:21:32 +0530

    Explore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./pages/Home/Home";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Explore from "./pages/Explore/Explore";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
         </Routes>
       </Router>
     </>
