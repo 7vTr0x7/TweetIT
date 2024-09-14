eded0fac9afc0accebb629082f102834213f002d
7vTr0x7 <7vTrox7@gmail.com>
2024-09-14 15:23:31 +0530

    Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Explore from "./pages/Explore/Explore";
 import Bookmark from "./pages/Bookmark/Bookmark";
+import Profile from "./pages/Profile/Profile";
 
 const App = () => {
   return (
