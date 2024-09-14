bfdc271399c735c9353a0df712526d1b3b70d9c8
7vTr0x7 <7vTrox7@gmail.com>
2024-09-14 15:42:13 +0530

    useFetchAllPosts

---
+++ frontend/src/hooks/useFetchAllPosts.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export const useFetchAllPosts = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/api/posts");
+      if (!res.ok) {
+        console.log("Failed to fetch posts");
+      }
+
+      const data = res.json();
+      setAllPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return allPosts;
+};
--- frontend/src/pages/Explore/Explore.jsx
+++ frontend/src/pages/Explore/Explore.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Nav from "../../components/Nav";
+import Header from "../../components/Header";
+import FollowSection from "../../components/FollowSection";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <>
+      <Header />
+      <div className="my-1 bg-body-tertiary container px-4">
+        <div className="row">
+          <div className="col-md-3">
+            <Nav />
+          </div>
+          <div className="col-md-6"></div>
+          <div className="col-md-3">
+            <FollowSection />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Explore;
