eef2c386ec0aaa1ba87161408863a46bf05d1e06
7vTr0x7 <7vTrox7@gmail.com>
2024-09-14 15:22:35 +0530

    Bookmark

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Explore from "./pages/Explore/Explore";
+import Bookmark from "./pages/Bookmark/Bookmark";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/explore" element={<Explore />} />
+          <Route path="/bookmark" element={<Bookmark />} />
         </Routes>
       </Router>
     </>
