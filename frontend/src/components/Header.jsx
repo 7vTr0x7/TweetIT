31ab215c780eff77bf334f258c1b55d37f6bcf56
7vTr0x7 <7vTrox7@gmail.com>
2024-09-14 15:28:32 +0530

    Routes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="container py-3">
       <h3 className="text-dark">
-        Tweet<span className="text-secondary">IT</span>
+        <Link to="/" className="nav-link">
+          Tweet<span className="text-secondary">IT</span>
+        </Link>
       </h3>
     </header>
   );
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,6 +3,7 @@ import { AiOutlineHome } from "react-icons/ai";
 import { MdOutlineExplore } from "react-icons/md";
 import { IoBookmarkOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
@@ -11,25 +12,43 @@ const Nav = () => {
         <ul
           className=" fw-semibold fs-6 "
           style={{ listStyle: "none", padding: "0px" }}>
-          <li className="mt-3">
+          <li className="mt-3 d-flex">
             <AiOutlineHome style={{ fontSize: "25px", marginBottom: "5px" }} />
-            <span className="px-3">Home</span>
+            <span className="px-3">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </span>
           </li>
-          <li className="mt-3">
+          <li className="mt-3 d-flex">
             <MdOutlineExplore
               style={{ fontSize: "25px", marginBottom: "5px" }}
             />
-            <span className=" px-3">Explore</span>
+            <span className=" px-3">
+              {" "}
+              <Link className="nav-link" to="/explore">
+                Explore
+              </Link>
+            </span>
           </li>
-          <li className="mt-3">
+          <li className="mt-3 d-flex">
             <IoBookmarkOutline
               style={{ fontSize: "25px", marginBottom: "5px" }}
             />
-            <span className=" px-3">Bookmark</span>
+            <span className=" px-3">
+              {" "}
+              <Link className="nav-link" to="/bookmark">
+                Bookmark
+              </Link>
+            </span>
           </li>
-          <li className="mt-3">
+          <li className="mt-3 d-flex ">
             <CgProfile style={{ fontSize: "25px", marginBottom: "5px" }} />
-            <span className=" px-3">Profile</span>
+            <span className=" px-3">
+              <Link className="nav-link" to="/profile">
+                Profile
+              </Link>
+            </span>
           </li>
         </ul>
       </nav>
